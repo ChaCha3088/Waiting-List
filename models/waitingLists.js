1b1c0cba7da32afc39898c9f48165b163080b93f
ChaCha3088 <cha3088@gmail.com>
2022-08-09 15:17:14 +0900

    tenth commit

--- models/waitingLists.js
+++ models/waitingLists.js
@@ -44,12 +44,24 @@ exports.countWaitingPeopleAll = () => {
 
 exports.createWaiting = (foodID, userID) => {
     //대기표 추가 기능
-    //대기표가 이미 존재하는지도 확인하고
+    //1. userID와 일치하는 대기표가 이미 존재하는지 확인
+    //이미 만들어진게 없다면, 이미 등록되었다는 알림창을 띄우고
+    //`/tickets/${foodID}/${userID}`로 redirect
+
+    //이미 만들어진게 없다면
     //waitingLists에 추가하는 함수
+    //대기 등록 버튼을 누르면 그 가게의 foodID와 userID, 그리고 Timestamp를 전달받아 waitingLists.js에 기록
+    //waitingLists 객체 안에 userIDAndTimestamp 안에 키와 밸류 형태로 넣습니다.
+    //키는 userID 밸류는 현재 날짜와 시간(yyyymmddHHmm)
     return 
 }
 
 
+
+exports.bringInfo = (foodID, userID) => {
+    //?waitingLists.js에서 가게 이름, 대기자 수 가져오기
+    //?foodID가 일치하는 것 찾고, 대기자 수 return
+}
 
 //{}객체
 //[]Array
--- routes/waitings.js
+++ routes/waitings.js
@@ -5,14 +5,13 @@ const WaitingLists = require('../models/waitingLists');
 
 router.get("/:foodID/:userID", (req, res, next) => {
     res.sendFile('waitings.html', { root: path.join(__dirname, '../public') });
-    //waitingLists.js에서 가게 이름 가져오기
-    //현재대기인원을 가져오는 waitingLists.js에 기록된 userID의 개수를 세는 model(현재대기인원 함수) 구현
-    //완료
-    
+
+
+
+    //WaitingLists.bringInfo()
+    //?waitingLists.js에서 가게 이름, 대기자 수 가져오기
+    //?foodID가 같은 것 찾고, 대기자 수 return
 });
-//식당 별 대기자 리스트 페이지 랜더
-// router.get("/waitings/:id/lists", );
-// 식당별 대기자 리스트 정보
 
 
 
@@ -22,13 +21,12 @@ router.post('/:foodID/:userID', (req, res, next) => {
     const userID = req.params.userID;
     const ticket = WaitingLists.createWaiting(foodID, userID);
     res.json(ticket);
+    res.redirect(`/tickets/${foodID}/${userID}`)
 });
 
 
 
-//대기 등록 버튼을 누르면 그 가게의 foodID와 userID, 그리고 Timestamp를 전달받아 waitingLists.js에 기록
-//router.post("/waitings/:userID", );
-//대기 등록 후 userID와 함께 tickets.js로 redirect
+
 
 //대기자로 이미 등록돼있으면 거절하는 기능도 추가해야함
 
