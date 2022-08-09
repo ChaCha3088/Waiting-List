8052c5e86e0d79666bf808e9a7be53f51bb932c1
ChaCha3088 <cha3088@gmail.com>
2022-08-09 14:11:49 +0900

    tenth commit

--- routes/lists.js
+++ routes/lists.js
@@ -27,16 +27,11 @@ router.get("/:userID", (req, res, next) => {
     res.json(waitingLists);
 
 
-
-
+    
+    //프론트엔드
     //1. 이 json 응답을
     //클라이언트가 받아서 가공하여 페이지에 넣는 코드 작성 요망
     
-
-
-
-    //waitingLists.js에 기록된 userID의 개수를 세는 model(현재대기인원 함수) 작성할 것
-
     //html에 있는 식당 버튼을 클릭하면 userID와 함께 waitings.js로 연결 구현
 });
 //메인, 식당 페이지 랜더
--- routes/waitings.js
+++ routes/waitings.js
@@ -7,6 +7,7 @@ router.get("/:foodID/:userID", (req, res, next) => {
     res.sendFile('waitings.html', { root: path.join(__dirname, '../public') });
     //waitingLists.js에서 가게 이름 가져오기
     //현재대기인원을 가져오는 waitingLists.js에 기록된 userID의 개수를 세는 model(현재대기인원 함수) 구현
+    //완료
     
 });
 //식당 별 대기자 리스트 페이지 랜더
